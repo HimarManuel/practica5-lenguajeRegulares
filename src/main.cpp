4eb9c992a802c1395c88d215f407fe0e52030fce
HimarManuel <xavieryanquin@gmail.com>
2018-11-13 00:05:50 +0000

    limpiado main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,96 +1,11 @@
 #include "../include/calculator_t.hpp"
 
 int main (void) {
-/*
-  string s;
-  cout << "Indica una cadena: ";
-  cin >> s;
-  chain_t chain1 (s);
-  chain_t chain2 ("adios");
 
-  cout << "Cadena 1: ";
-  chain1.write();
-  cout << "\n";
+  calculator_t cal;
 
-  cout << "Cadena 2: ";
-  chain2.write();
-  cout << "\n";
-
-  cout << "Concatenacion: ";
-  chain1.concatenation(chain2).write();
-  cout << "\n";
-
-  cout << "Potencia de la cadena 1 elevada a 3: ";
-  chain1.power(3).write();
-  cout << "\n";
-
-  cout << "Inversa de la cadena 2: ";
-  chain2.reverse().write();
-  cout << "\n";
-  */
-
-/*
-  string s;
-  cout << "Indica un lenguaje: ";
-  cin >> s;
-  language_t l1;
-  l1 = l1.split(s);
-  cout << "Indica otro lenguaje: ";
-  cin >> s;
-  language_t l2;
-  l2 = l2.split(s);
-  int num;
-  cout << "Indica un valor: ";
-  cin >> num;
-  cout << "¿Los lenguajes son iguales?: " << l1.is_equal(l2) << "\n";
-
-  cout << "Derecho: ";
-  l1.write();
-  cout << "\n";
-  cout << "Reves: ";
-  l1.reverse().write();
-  cout << "\n";
-
-  cout << "l1 elevado a " << num << ": ";
-  l1.power(num).write();
-  cout << "\n";
-
-  cout << "Concatenacion con l2: ";
-  l1.concatenation(l2).write();
-  cout << "\n";
-
-  cout << "Union de las dos cadenas: ";
-  l1.join(l2).write();
-  cout << "\n";
-
-  cout << "Interseccion de las dos cadenas: ";
-  l1.intersection(l2).write();
-  cout << "\n";
-
-  cout << "Diferencia entre l1 y l2: ";
-  l1.difference(l2).write();
-  cout << "\n";
-
-  cout << "l2 es sublenguaje de l1? " << l1.is_sublanguage(l2) << "\n";
-*/
-/*while (true) {
-  cout << "----------------------------------------------\n";
-  string s;
-  cin >> s;
-  language_t l1(s);
-  cout << "Resultado: "; l1.write(); cout << "\n";
-}*/
-/*
-string s;
-cin >> s;
-language_t l1;
-l1 = l1.split(s);
-l1.kleene(INF_LIMIT).write();*/
-
-calculator_t cal;
-
-int opt;
-while (opt != 10) {
+  int opt;
+  while (opt != 10) {
     cout << "1. Inversa\n";
     cout << "2. Concatenacion\n";
     cout << "3. Union\n";
